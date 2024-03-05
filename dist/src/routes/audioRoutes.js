3ba58ce92a13372adf99294be4c0e37be50aaa04
smithwithatypo <smithwithatypo@gmail.com>
2024-03-05 15:39:22 -0600

    Stop tracking dist/ folder

--- dist/src/routes/audioRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import upload from '../middleware/multerConfig.js';
-import { AudioController } from '../controllers/audioController.js';
-const router = express.Router();
-//routes
-router.post('/upload-audio', upload.single('audio'), AudioController.uploadAudio);
-export default router;
